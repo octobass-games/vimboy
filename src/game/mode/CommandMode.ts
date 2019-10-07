e88e0344c0c968ed5c17e018976db35a0dcfe4e7
Adam Wright <adam@Adams-MacBook-Pro.local>
2019-10-07 22:34:40 +0100

    whitelist CommandMode inputs

--- src/game/mode/CommandMode.ts
+++ src/game/mode/CommandMode.ts
@@ -2,6 +2,13 @@ import Mode from './Mode';
 import EnterNormalMode from './action/EnterNormalMode';
 
 class CommandMode extends Mode {
+    private static whitelist: string[] = [
+        'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
+        'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
+        'u', 'v', 'w', 'x', 'y', 'z', '0', '1', '2', '3',
+        '4', '5', '6', '7', '8', '9'
+    ];
+
     private input: string = '';
 
     constructor() {
@@ -23,7 +30,7 @@ class CommandMode extends Mode {
             this.input = this.input.slice(0, this.input.length - 1);
         } else if (keyEvent.key === 'Escape') {
             new EnterNormalMode().act(context);
-        } else {
+        } else if (CommandMode.whitelist.includes(keyEvent.key)) {
             this.input = this.input.concat(keyEvent.key);
         }
     }
