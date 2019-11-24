32c5742f3803c362607a17777eddf2604e643ce9
majicmoo <megan@theparkes.me.uk>
2019-11-24 22:31:01 +0000

    make menu better

--- src/game/scene/Menu.ts
+++ src/game/scene/Menu.ts
@@ -1,8 +1,9 @@
-import Phaser from "phaser";
-import { play } from "../Game";
+import Phaser, { GameObjects } from "phaser";
 import WebFont from "webfontloader";
-import { GAME_HEIGHT, GAME_WIDTH } from "../../constants/game";
-import { Colours } from "../../constants/colours";
+import { GAME_HEIGHT, GAME_WIDTH, CELL_SIZE } from "../../constants/game";
+import { Colours, StringColours } from "../../constants/colours";
+import { FONT, FONT_SIZE } from "../../constants/text";
+import { handleInput } from "../../menu/handleInput";
 
 const sceneConfig: Phaser.Types.Scenes.SettingsConfig = {
   active: false,
@@ -11,6 +12,15 @@ const sceneConfig: Phaser.Types.Scenes.SettingsConfig = {
 };
 
 export class Menu extends Phaser.Scene {
+  public history: string[] = [];
+  public historyPosition = 0;
+  public currentLine: string = "";
+  public lineText?: GameObjects.Text;
+  public header?: GameObjects.Text;
+  public preamble?: GameObjects.Text;
+  public cursor?: GameObjects.Rectangle;
+  public historyObj?: GameObjects.Group;
+
   constructor() {
     super(sceneConfig);
   }
@@ -38,20 +48,49 @@ export class Menu extends Phaser.Scene {
       active: () => {
         loadingText.destroy();
         rect.destroy();
+        this.onLoaded();
       }
     });
   }
 
-  public create() {
-    const helloButton = this.add.text(100, 100, "Play", {
-      fill: "#0f0"
+  private onLoaded = () => {
+    this.historyObj = this.add.group();
+    this.header = this.add.text(0, 0, "vimboy@octobass.games", {
+      fontFamily: FONT,
+      fontSize: FONT_SIZE,
+      color: StringColours.PINK
     });
-    helloButton.setInteractive();
 
-    helloButton.on("pointerup", () => {
-      play();
+    this.preamble = this.add.text(0, FONT_SIZE, "~", {
+      fontFamily: FONT,
+      fontSize: FONT_SIZE,
+      color: StringColours.YELLOW
     });
-  }
+
+    this.lineText = this.add.text(30, FONT_SIZE, "", {
+      fontFamily: FONT,
+      fontSize: FONT_SIZE
+    });
+
+    this.cursor = this.add.rectangle(
+      30,
+      FONT_SIZE + CELL_SIZE / 2,
+      5,
+      CELL_SIZE,
+      Colours.WHITE
+    );
+    this.tweens.add({
+      targets: this.cursor,
+      alpha: { from: 0, to: 1 },
+      ease: "Cubic",
+      duration: 1000,
+      repeat: -1, // -1: infinity
+      yoyo: false
+    });
+    this.input.keyboard.on("keydown", handleInput(this));
+  };
+
+  public create() {}
 
   public update() {}
 }
---
+++ src/menu/handleInput.ts
@@ -0,0 +1,83 @@
+import { Menu } from "../game/scene/Menu";
+import { play } from "../game/Game";
+import { FONT, FONT_SIZE } from "../constants/text";
+import { PADDING } from "./constants";
+import { CELL_SIZE } from "../constants/game";
+
+export const handleInput = (menu: Menu) => (keyEvent: KeyboardEvent) => {
+  switch (keyEvent.key) {
+    case "Enter":
+      handleEnter(menu);
+      break;
+    case "Backspace":
+      handleDelete(menu);
+      break;
+    case "ArrowUp":
+      handleHistoryLoop(menu, 1, 0);
+      break;
+    case "ArrowDown":
+      handleHistoryLoop(menu, -1, menu.history.length - 1);
+      break;
+    default:
+      menu.currentLine += keyEvent.key;
+      menu.lineText!.setText(menu.currentLine);
+  }
+
+  menu.lineText!.setText(menu.currentLine);
+  menu.cursor!.setX(menu.currentLine.length * 17 + 30);
+};
+
+const handleHistoryLoop = (
+  menu: Menu,
+  modifier: number,
+  initialiser: number
+) => {
+  const history = [...menu.history].reverse();
+  const entry = history[menu.historyPosition];
+  if (entry === undefined) {
+    if (menu.historyPosition !== 0) {
+      menu.historyPosition = initialiser;
+      menu.currentLine = history[initialiser] || "";
+    }
+  } else {
+    menu.currentLine = entry;
+    if (menu.historyPosition + modifier === -1) {
+      menu.historyPosition = history.length - 1;
+    } else {
+      menu.historyPosition += modifier;
+    }
+  }
+};
+
+const playWords = ["play", "vim", "vi"];
+const handleCommand = (menu: Menu, command: string) => {
+  if (playWords.includes(command.toLowerCase())) {
+    play();
+  }
+};
+
+const handleDelete = (menu: Menu) => {
+  if (menu.currentLine.length > 0) {
+    menu.currentLine = menu.currentLine.slice(0, menu.currentLine.length - 1);
+  }
+};
+
+const handleEnter = (menu: Menu) => {
+  const y = menu.historyObj!.getChildren().length + 1;
+
+  handleCommand(menu, menu.currentLine);
+
+  menu.history.push(menu.currentLine);
+  menu.historyObj!.add(
+    menu.add.text(PADDING, (y - 1) * FONT_SIZE, menu.currentLine, {
+      fontFamily: FONT,
+      fontSize: FONT_SIZE
+    })
+  );
+  menu.currentLine = "";
+  menu.lineText!.setY((y + 1) * FONT_SIZE);
+  menu.cursor!.setY((y + 1) * FONT_SIZE + CELL_SIZE / 2);
+  menu.preamble!.setY((y + 1) * FONT_SIZE);
+  menu.header!.setY(y * FONT_SIZE);
+  menu.historyPosition = 0;
+};
