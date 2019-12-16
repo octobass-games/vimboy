e85fe4817eb88afa48db3caccd6ef67d704e4f8d
majicmoo <megan@theparkes.me.uk>
2019-12-16 22:16:14 +0000

    add jump tutorial

--- src/game/tutorial/TutorialCommands.ts
+++ src/game/tutorial/TutorialCommands.ts
@@ -1,11 +1,12 @@
 import { printWord } from "./tutorialUtils";
 
 class TutorialCommands {
+  private input = "";
   public init = () => {
     window.scene.time.addEvent({
       delay: 1000,
       callback: () => {
-        printWord(["typing a colon will enter you into command mode"], 4);
+        printWord(["Typing a colon will enter you into command mode"], 4);
       }
     });
 
@@ -21,8 +22,35 @@ class TutorialCommands {
         window.scene.modeManager.allow(
           ...Array.from(Array(10).keys()).map(v => v.toString())
         );
+        window.scene.keyCapturer!.addListener("keydown", this.jump);
       }
     });
+  };
+
+  private jump = (keyEvent: KeyboardEvent) => {
+    if (keyEvent.key.toLowerCase() === "enter") {
+      if (this.input.match(/:[0-9]+/) !== null) {
+        window.scene.entityManager.destroyAll();
+        window.scene.keyCapturer!.removeListener("keydown", this.jump);
+        printWord(["Look at you go!"], 1);
+
+        window.scene.time.addEvent({
+          delay: 1000,
+          callback: () => {
+            printWord(
+              [
+                "Theres all sorts of commands so feel free to try whatever you can think of!"
+              ],
+              2
+            );
+          }
+        });
+      } else {
+        this.input = "";
+      }
+    } else {
+      this.input += keyEvent.key.toLowerCase();
+    }
   };
 
   public update = () => {};
