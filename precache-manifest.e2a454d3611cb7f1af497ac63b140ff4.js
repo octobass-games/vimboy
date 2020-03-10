95f8607e474c0d89973f378a3eb88fa16d26ce26
majicmoo <megan@theparkes.me.uk>
2020-03-10 21:05:48 +0000

    Updates

---
+++ precache-manifest.e2a454d3611cb7f1af497ac63b140ff4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46b36d92687376e95a9e5ebf7ed0af46",
+    "url": "/vimboy/index.html"
+  },
+  {
+    "revision": "f88256d05e03c1dd7e4e",
+    "url": "/vimboy/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "e36e41fbf9c1866ddee1",
+    "url": "/vimboy/static/js/2.9980fcf4.chunk.js"
+  },
+  {
+    "revision": "f88256d05e03c1dd7e4e",
+    "url": "/vimboy/static/js/main.333001a2.chunk.js"
+  },
+  {
+    "revision": "519cef13459391921d48",
+    "url": "/vimboy/static/js/runtime-main.38c5fd54.js"
+  }
+]);
